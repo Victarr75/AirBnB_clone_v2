42c63f4138fa2ea15fbfc69b466b8c58174c7970
Victarr75 <korirvictor75@gmail.com>
2024-07-11 12:16:01 +0300

    server setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,41 +1,38 @@
 #!/usr/bin/env bash
-# This script sets up web servers for deployment of web_static.
+# Sets up a web server for deployment of web_static.
 
-# Install Nginx if not already installed
-if ! dpkg -s nginx &> /dev/null; then
-    sudo apt-get -y update
-    sudo apt-get -y install nginx
-fi
+apt-get update
+apt-get install -y nginx
 
-# Create necessary directories
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Create a fake HTML file for testing
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-# Create or recreate symbolic link
-if [[ -L /data/web_static/current ]]; then
-    sudo rm /data/web_static/current
-fi
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-# Set ownership of directories
-sudo chown -R ubuntu:ubuntu /data/
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
 
-# Update Nginx configuration
-config_file="/etc/nginx/sites-available/default"
-sudo sed -i '/^\tlocation \/hbnb_static\/ {$/a \
-\t\talias /data/web_static/current/;\n' "$config_file"
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
 
-# Restart Nginx
-sudo service nginx restart
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 
-# Exit successfully
-exit 0
+service nginx restart
